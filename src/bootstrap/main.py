e81de41a2035b8007733f2e3c81bb423cb19ae4b
Sapein <5852983+Sapein@users.noreply.github.com>
2022-03-23 16:01:39 +0000

    Actually have it compile other files.

--- src/bootstrap/main.py
+++ src/bootstrap/main.py
@@ -2,6 +2,8 @@
 This module is the 'main' module for the bootstrap compiler.
 """
 
+from sys import argv
+from typing import Optional
 
 import tokens
 
@@ -15,9 +17,14 @@ import pass6
 import compiler
 
 
-with open('test.shisp', 'r') as f:
-    tokenized = tokens.parse_file(f.read())
-    ast = parser.parse_tokens(tokenized)
+def run_compiler(file_name: str, output_file: Optional[str] = None): 
+    try:
+        with open(file_name, 'r') as f:
+            tokenized = tokens.parse_file(f.read())
+    except FileNotFoundError:
+        print("File {} not found!".format(file_name))
+        return
+    ast = parser.parse_tokens(tokenized, '.'.join(file_name.split('.')[:-1]))
     ast.print_ast()
     ast = pass2.squash_ast(ast)
     ast = pass3.resolve_metamacros(ast)
@@ -25,5 +32,18 @@ with open('test.shisp', 'r') as f:
     ast = pass5.check_variables(ast)
     ast = pass6.replace_references(ast)
     output = compiler.compile(ast)
-    with open('test.sh', 'w') as f:
+    if not output_file:
+        output_file = '{}.sh'.format(ast.program_name)
+    with open(output_file,'w') as f:
         f.write(output)
+
+def compiler_help():
+    return 'usage: python3 main.py file (argument)'
+
+match argv:
+    case [a, b]:
+        run_compiler(b)
+    case [a, b, c]:
+        run_compiler(b, c)
+    case [_] | _:
+        print(compiler_help())
