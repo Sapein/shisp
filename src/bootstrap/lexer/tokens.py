fb19351f765d9f3428e5c2d999b5a10db4846f6d
Sapein <5852983+Sapein@users.noreply.github.com>
2022-03-31 15:00:46 -0500

    Improve lexer/tokens.py.

--- src/bootstrap/lexer/tokens.py
+++ src/bootstrap/lexer/tokens.py
@@ -1,20 +1,28 @@
 """
-This is the tokenizer.
+Shisp Tokenizer. 
 """
 
 from dataclasses import dataclass
 
 @dataclass(frozen=True, repr=True)
 class Token:
+    """
+    A basic dataclass representing a token.
+    """
     row: int
     column: int
     char: str
 
-def parse_file(text: str):
+
+def parse_file(text: str) -> list[Token]:
+    """
+    Parse a file into a list of tokens.
+    """
     text = text.replace('\r', '').replace('\n', '\n\r')
     lines = text.split('\r')
     tokenized = []
     for row, line in enumerate(lines, start=1):
         for column, char in enumerate(line, start=1):
             tokenized.append(Token(row, column, char))
+
     return tokenized
